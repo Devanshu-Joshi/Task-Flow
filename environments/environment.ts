593b6be08720d571badf6f1f6fd55ae71122cd52
Devanshu Joshi <iamdevanshu007@gmail.com>
2026-02-05 18:45:48 +0530

    fix: update API_URL and enhance due date input styling in task form

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // API_URL: 'http://10.251.57.23:3080/api',
-    API_URL: 'http://localhost:3080/api',
+    API_URL: 'http://10.251.57.207:3080/api',
+    // API_URL: 'http://localhost:3080/api',
     production: false
 };
